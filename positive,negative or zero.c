93fbe4c2374cd0241468392f0d6415503cc5e32b
manimekalaivadivel <30343895+manimekalaivadivel@users.noreply.github.com>
2017-08-12 17:03:19 +0530

    Create positive,negative or zero.c

---
+++ positive,negative or zero.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+printf("enter a number");
+scanf("%d",&n);
+if(n==0)
+{
+printf("tne number is zero");
+}
+elseif(n<0)
+{
+printf("the number is negative");
+}
+else
+{
+printf("the number is positive");
+getch();
+}
